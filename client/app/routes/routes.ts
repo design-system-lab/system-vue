32e0196c6c135efb9d5e258d4756bdb097757643
Josh Kriese <joshuakriese@gmail.com>
2023-01-21 15:15:12 -0500

    Add icon component for housing icons

--- client/app/routes/routes.ts
+++ client/app/routes/routes.ts
@@ -1,7 +1,9 @@
 import Home from '../pages/HomePage.vue';
 import ButtonPage from '../pages/ButtonPage.vue';
+import IconPage from '../pages/IconPage.vue';
 
 export default [
   { path: '/', name: 'Home', component: Home },
   { path: '/buttons', name: 'Buttons', component: ButtonPage },
+  { path: '/icons', name: 'Icons', component: IconPage },
 ]
