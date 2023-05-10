aa467a77271860b26b17c935a0db8bb93ef71ce9
Josh Kriese <joshuakriese@gmail.com>
2023-05-09 21:43:52 -0400

    set up initial files

---
+++ src/components/Form/FdInputField.vue
@@ -0,0 +1,45 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+interface ErrorMessages {
+  [key: string]: string; 
+}
+
+export default defineComponent({
+  name: 'FdInputField',
+  props: {
+    label: {
+      type: String,
+      default: undefined,
+    },
+    errorMessages: {
+      type: Object as PropType<ErrorMessages>,
+      default: () => ({}),
+    },
+    errors: {
+      type: Array as PropType<string[]>,
+      default: () => [],
+    },
+    assistiveText: {
+      type: String,
+      default: undefined,
+    },
+    placeholder: {
+      type: String,
+      default: undefined,
+    },
+    modelValue: {
+      type: String,
+      default: undefined,
+    },
+  }
+});
+</script>
+
+<style>
+
+</style>
