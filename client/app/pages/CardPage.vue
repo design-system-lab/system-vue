a5ad984ce7b47e0b53a783523b965cbd50110988
Josh Kriese <joshuakriese@gmail.com>
2024-12-19 10:28:14 -0500

    Build card component!

---
+++ client/app/pages/CardPage.vue
@@ -0,0 +1,192 @@
+<template>
+  <div>
+    <h1>Chip</h1>
+    <br>
+    <div class="control-panel">
+      <div>
+        <fd-input-field
+          v-model="overlineText"
+          id="overline"
+          class="mb-4"
+          label="Overline Text"
+          small
+        />
+        <fd-input-field
+          v-model="titleText"
+          id="title"
+          class="mb-4"
+          label="Title Text"
+          small
+        />
+        <fd-input-field
+          v-model="contentText"
+          id="content"
+          class="mb-4"
+          label="Content Text"
+          small
+        />
+        <fd-input-field
+          v-model="actionText"
+          id="action"
+          class="mb-4"
+          label="Action Text"
+          small
+        />
+        <fd-input-field
+          v-model="bg"
+          id="bg"
+          label="Background Color"
+          small
+        />
+      </div>
+      <div>
+        <fd-group
+          id="options"
+          type="checkbox"
+        >
+          <template #label>
+            Options
+          </template>
+          <fd-checkbox
+            v-model="showImage"
+            id="show-image"
+            value="show-image"
+          >Show Image</fd-checkbox>
+          <fd-checkbox
+            v-model="showAction"
+            id="show-action"
+            value="show-action"
+          >Show Action</fd-checkbox>
+        </fd-group>
+      </div>
+    </div>
+
+    <div
+      class="display-panel mb-4"
+      :style="{ backgroundColor: bg }"
+    >
+      <fd-row>
+        <fd-col>
+          <fd-card>
+            <template v-if="showImage" #image>
+              <img src="https://placehold.co/600x400" alt="Placeholder" />
+            </template>
+            <template #overline>
+              {{ overlineText }}
+            </template>
+            <template #title>
+              {{ titleText }}
+            </template>
+            <template #content>
+              {{ contentText }}
+            </template>
+            <template v-if="showAction" #action>
+              <fd-button>
+                {{ actionText }}
+              </fd-button>
+            </template>
+          </fd-card>
+        </fd-col>
+        <fd-col>
+          <fd-card>
+            <p>
+              Example of a card fully overwritten with custom content.
+            </p>
+
+            <fd-input-field
+              v-model="exampleInput"
+              id="example-input"
+              label="Example Input"
+            />
+
+            <fd-input-field
+              v-model="exampleInput"
+              id="example-input"
+              label="Example Input"
+            />
+
+            <fd-input-field
+              v-model="exampleInput"
+              id="example-input"
+              label="Example Input"
+            />
+
+            <fd-button>
+              Example Button
+            </fd-button>
+          </fd-card>
+        </fd-col>
+      </fd-row>
+    </div>
+
+    <!-- add custom icon version -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, shallowRef } from 'vue';
+import { CubeTransparentIcon } from '@heroicons/vue/24/solid'
+import FdAlert from '../../../src/components/Alert';
+import FdButton from '../../../src/components/Button';
+import FdCard from '../../../src/components/Card';
+import FdCheckbox from '../../../src/components/Checkbox';
+import FdGroup from '../../../src/components/Group';
+import { FdInputField } from '../../../src/components/Form';
+import FdRow from '../../../src/components/Row';
+import FdCol from '../../../src/components/Col';
+
+export default defineComponent({
+  name: 'AlertPage',
+  components: { FdAlert, FdButton, FdCard, FdCheckbox, FdCol, FdGroup, FdInputField, FdRow },
+  setup() {
+    const actionText = shallowRef('Action Text');
+    const exampleInput = shallowRef('');
+    const overlineText = shallowRef('Overline Text');
+    const showAction = shallowRef(true);
+    const showImage = shallowRef(true);
+    const titleText = shallowRef('Title Text');
+    const contentText = shallowRef('Dive into the exciting realm of programming with our comprehensive courses. Whether you\'re a beginner or looking to enhance your skills, we offer a range of tutorials and projects to help you succeed.');
+
+    const bg = shallowRef('#fff');
+
+    return {
+      actionText,
+      bg,
+      exampleInput,
+      overlineText,
+      showAction,
+      showImage,
+      contentText,
+      titleText,
+      CubeTransparentIcon,
+    };
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/required";
+
+.control-panel {
+  display: flex;
+  gap: 32px;
+  padding: 1.5rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+}
+
+.display-panel {
+  margin-top: 0.5rem;
+  padding: 1.5rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.aux-text {
+  font-size: $font-sm;
+  color: rgba(var(--fora_neutral-8), 1);
+}
+</style>
---
+++ src/components/Card/FdCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="fd-card">
+    <slot>
+      <div class="fd-card__image">
+        <slot name="image"></slot>
+      </div>
+      <div class="fd-card__content-container">
+        <div class="fd-card__header">
+          <slot name="header">
+            <div class="fd-card__overline">
+              <slot name="overline"></slot>
+            </div>
+            <h5 class="fd-card__title">
+              <slot name="title"></slot>
+            </h5>
+          </slot>
+        </div>
+        <div class="fd-card__content">
+          <slot name="content"></slot>
+        </div>
+      </div>
+      <div class="fd-card__action">
+        <slot name="action"></slot>
+      </div>
+    </slot>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FdCard",
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/required";
+
+.fd-card {
+  background-color: rgba(var(--fora_card_bg));
+  border: $card_border rgba(var(--fora_card_border-color));
+  border-radius: $card_border-radius;
+  box-shadow: $card_box-shadow;
+  display: flex;
+  flex-direction: column;
+  gap: $card_gap;
+  padding: $card_padding;
+  width: 100%;
+
+  &__image {
+    border-radius: $card_image_border-radius;
+    overflow: hidden;
+  }
+
+  &__content-container {
+    display: flex;
+    flex-direction: column;
+    gap: $card_content_gap;
+  }
+
+  &__header {
+    display: flex;
+    flex-direction: column;
+    gap: $card_header_gap;
+  }
+
+  &__overline {
+    color: rgba(var(--fora_card_overline_color));
+    font-size: $card_overline_font-size;
+    font-weight: $card_overline_font-weight;
+    line-height: $card_overline_line-height;
+  }
+
+  &__title {
+    color: rgba(var(--fora_card_title_color));
+    font-size: $card_title_font-size;
+    font-weight: $card_title_font-weight;
+    line-height: $card_title_line-height;
+  }
+
+  &__content {
+    color: rgba(var(--fora_card_content_color));
+    font-size: $card_content_font-size;
+    font-weight: $card_content_font-weight;
+    line-height: $card_content_line-height;
+  }
+
+  &__action {
+    display: flex;
+    gap: $card_action_gap;
+  }
+}
+</style>
